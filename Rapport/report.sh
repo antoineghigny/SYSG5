d7442a4e3cb908a8b776fd453f7a562960cc6622
antoineghigny <antoine@ghigny.be>
2022-11-17 11:58:50 +0100

    add script to compile pdf

---
+++ Rapport/report.sh
@@ -0,0 +1,9 @@
+pdflatex SYSG5\ -\ Failles\ de\ sécurité\ linux.tex 1 > /dev/null
+bibtex SYSG5\ -\ Failles\ de\ sécurité\ linux
+pdflatex SYSG5\ -\ Failles\ de\ sécurité\ linux.tex 1 > /dev/null
+rm *.aux 
+rm *.out 
+rm *.toc 
+rm *.log 
+rm *.bbl 
+rm *.blg 
