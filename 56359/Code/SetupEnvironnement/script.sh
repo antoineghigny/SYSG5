bd9b6bcb7b5db8efb0e79d0c9ae955473c41f31c
antoineghigny <58885171+antoineghigny@users.noreply.github.com>
2022-11-30 00:22:08 +0100

    Update script.sh

--- 56359/Code/SetupEnvironnement/script.sh
+++ 56359/Code/SetupEnvironnement/script.sh
@@ -1,12 +1,9 @@
 
-sudo bash -c 'echo "deb http://deb.debian.org/debian bullseye main" > /etc/apt/sources.list'
-sudo bash -c 'echo "deb-src http://deb.debian.org/debian bullseye main" >> /etc/apt/sources.list'
+sudo bash -c 'deb http://security.debian.org/debian-security buster/updates main contrib" >> /etc/apt/sources.list'
+sudo bash -c 'deb-src http://security.debian.org/debian-security buster/updates main contrib" >> /etc/apt/sources.list'
 
-sudo bash -c 'echo "deb http://deb.debian.org/debian-security/ bullseye-security main" >> /etc/apt/sources.list'
-sudo bash -c 'echo "deb-src http://deb.debian.org/debian-security/ bullseye-security main" >> /etc/apt/sources.list'
-
-sudo bash -c 'echo "deb http://deb.debian.org/debian bullseye-updates main" >> /etc/apt/sources.list'
-sudo bash -c 'echo "deb-src http://deb.debian.org/debian bullseye-updates main" >> /etc/apt/sources.list'
+sudo bash -c 'deb http://deb.debian.org/ buster-updates main contrib" >> /etc/apt/sources.list'
+sudo bash -c 'deb-src http://ftp.debian.org/debian/ buster main contrib" >> /etc/apt/sources.list'
 
 sudo apt -y update
 sudo apt -y install git
