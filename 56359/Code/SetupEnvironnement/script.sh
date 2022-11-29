a0f92426e868477d9230f01a393e8f73f6b7ef38
antoineghigny <58885171+antoineghigny@users.noreply.github.com>
2022-11-30 00:25:55 +0100

    Update script.sh

--- 56359/Code/SetupEnvironnement/script.sh
+++ 56359/Code/SetupEnvironnement/script.sh
@@ -1,9 +1,9 @@
 
-sudo bash -c 'deb http://security.debian.org/debian-security buster/updates main contrib" >> /etc/apt/sources.list'
-sudo bash -c 'deb-src http://security.debian.org/debian-security buster/updates main contrib" >> /etc/apt/sources.list'
+sudo bash -c 'echo "deb http://security.debian.org/debian-security buster/updates main contrib" > /etc/apt/sources.list'
+sudo bash -c 'echo "deb-src http://security.debian.org/debian-security buster/updates main contrib" >> /etc/apt/sources.list'
 
-sudo bash -c 'deb http://deb.debian.org/ buster-updates main contrib" >> /etc/apt/sources.list'
-sudo bash -c 'deb-src http://ftp.debian.org/debian/ buster main contrib" >> /etc/apt/sources.list'
+sudo bash -c 'echo "deb http://deb.debian.org/ buster-updates main contrib" >> /etc/apt/sources.list'
+sudo bash -c 'echo "deb-src http://ftp.debian.org/debian/ buster main contrib" >> /etc/apt/sources.list'
 
 sudo apt -y update
 sudo apt -y install git
