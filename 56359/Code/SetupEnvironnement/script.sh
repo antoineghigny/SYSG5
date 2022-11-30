5002640db951e066a9d5203e389ad3835da501d0
antoineghigny <58885171+antoineghigny@users.noreply.github.com>
2022-11-30 21:25:58 +0100

    Update script.sh

--- 56359/Code/SetupEnvironnement/script.sh
+++ 56359/Code/SetupEnvironnement/script.sh
@@ -2,7 +2,11 @@
 sudo bash -c 'echo "deb http://security.debian.org/debian-security buster/updates main contrib" > /etc/apt/sources.list'
 sudo bash -c 'echo "deb-src http://security.debian.org/debian-security buster/updates main contrib" >> /etc/apt/sources.list'
 
-sudo bash -c 'echo "deb http://deb.debian.org/ buster-updates main contrib" >> /etc/apt/sources.list'
+sudo bash -c 'echo "deb http://deb.debian.org/debian/ buster-updates main contrib" >> /etc/apt/sources.list'
+sudo bash -c 'echo "deb-src htp://deb.debian.org/debian/ buster-updates main contrib" >> /etc/apt/sources.list'
+
+
+sudo bash -c 'echo "http://ftp.debian.org/debian/ buster main contrib" >> /etc/apt/sources.list'
 sudo bash -c 'echo "deb-src http://ftp.debian.org/debian/ buster main contrib" >> /etc/apt/sources.list'
 
 sudo apt -y update
