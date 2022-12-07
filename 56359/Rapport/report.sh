96edd38fd12d811a4ceae084b223609e8dcb172b
56359 <56359@etu.he2b.be>
2022-12-07 15:45:14 +0100

    refractor folders

--- 56359/Rapport/report.sh
+++ 56359/Rapport/report.sh
@@ -2,6 +2,7 @@ pdflatex SYSG5\ -\ Failles\ de\ sécurité\ linux.tex
 bibtex SYSG5\ -\ Failles\ de\ sécurité\ linux
 pdflatex SYSG5\ -\ Failles\ de\ sécurité\ linux.tex
 pdflatex SYSG5\ -\ Failles\ de\ sécurité\ linux.tex
+
 rm *.out
 rm *.toc
 rm *.bbl
