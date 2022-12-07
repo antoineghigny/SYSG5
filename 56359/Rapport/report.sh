9144a7c0a4088bcac09e4889974fe8448035cb4f
56359 <56359@etu.he2b.be>
2022-12-07 12:43:35 +0100

    update script report

--- 56359/Rapport/report.sh
+++ 56359/Rapport/report.sh
@@ -1,3 +1,10 @@
 pdflatex SYSG5\ -\ Failles\ de\ sécurité\ linux.tex
 bibtex SYSG5\ -\ Failles\ de\ sécurité\ linux
 pdflatex SYSG5\ -\ Failles\ de\ sécurité\ linux.tex
+pdflatex SYSG5\ -\ Failles\ de\ sécurité\ linux.tex
+rm *.out
+rm *.toc
+rm *.bbl
+rm *.blg
+rm *.log
+rm *.aux
